2774c04f01d1ae75affc10c2060629d1c43f1726
Giuseppe Valeriani <giuggey85@gmail.com>
2024-01-24 20:41:35 +0000

    add some seed for initial data

--- seeds/children.js
+++ seeds/children.js
@@ -1,9 +1,4 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
   await knex("children").del();
   await knex("children").insert([
     { id: 1, name: "romolo", current_points: 40 },
--- seeds/tasks.js
+++ seeds/tasks.js
@@ -1,39 +1,29 @@
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
   await knex("tasks").del();
   await knex("tasks").insert([
     {
       id: 1,
-      child_id: 1,
       task: "brush your teeth",
       frequence: "daily",
       icon: "icons/kidschores-23.png",
       points: 5,
       is_skill: false,
-      is_active: true,
-      is_completed: false,
     },
     {
       id: 2,
-      child_id: 1,
       task: "walk the dog",
       frequence: "daily",
       icon: "icons/kidschores-86.png",
       points: 5,
       is_skill: false,
-      is_active: true,
-      is_completed: false,
     },
     {
       id: 3,
-      child_id: 2,
       task: "brush your teeth",
       frequence: "daily",
       icon: "icons/lemonadepixel_kidschores-23.png",
       points: 5,
       is_skill: false,
-      is_active: true,
-      is_completed: false,
     },
   ]);
 };
