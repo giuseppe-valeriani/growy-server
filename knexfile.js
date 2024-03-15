b9d3ef5a9971ac1001f69799ae3f22bc1a6d2028
Giuseppe Valeriani <giuggey85@gmail.com>
2024-03-15 09:26:13 +0000

    update env names

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,9 @@ module.exports = {
   client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
-    database: process.env.DB_LOCAL_DBNAME,
-    user: process.env.DB_LOCAL_USER,
-    password: process.env.DB_LOCAL_PASSWORD,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     charset: "utf8",
   },
 };
