ed5340783ffa36c378a4542b7a4779fa63586ab5
Giuseppe Valeriani <giuggey85@gmail.com>
2024-04-09 20:41:28 +0100

    adjust .env

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     charset: "utf8",
