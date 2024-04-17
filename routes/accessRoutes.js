fcb8d23f733e4db6f03e98e15aef3c7add2ccc90
Giuseppe Valeriani <giuggey85@gmail.com>
2024-04-17 16:11:58 +0100

    add error handling on login

--- routes/accessRoutes.js
+++ routes/accessRoutes.js
@@ -25,20 +25,24 @@ const validationList = [
 ];
 
 router.post("/login", async (req, res) => {
-  if (!req.body.user || !req.body.password) {
-    return res
-      .status(400)
-      .json({ message: "Please provide valid user and password" });
-  }
-  if (
-    validationList.find(
-      (user) =>
-        user.user === req.body.user && user.password === req.body.password
-    )
-  ) {
-    const user = { name: req.body.user };
-    const accessToken = generateToken(user);
-    return res.json({ accessToken: accessToken });
+  try {
+    if (!req.body.user || !req.body.password) {
+      return res
+        .status(400)
+        .json({ message: "Please provide valid user and password" });
+    }
+    if (
+      validationList.find(
+        (user) =>
+          user.user === req.body.user && user.password === req.body.password
+      )
+    ) {
+      const user = { name: req.body.user };
+      const accessToken = generateToken(user);
+      return res.json({ accessToken: accessToken });
+    } else return res.status(403).json({ message: "Wrong credentials" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 });
 
